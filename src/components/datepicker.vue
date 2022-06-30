1bd8840dc6fc40f62216c5ba7224322e1101ce6c
GeorgeC <g.caliment@reply.it>
2022-06-30 02:39:58 +0200

    fixed datepicker disabledStartDateCalc

--- src/components/datepicker.vue
+++ src/components/datepicker.vue
@@ -214,7 +214,7 @@ export default {
             ? disabledDate
             : this.disabledStartDate.from
         unSelectedDate.from = disabledDate
-        unSelectedDate.to = this.disabledStartDate.from
+        unSelectedDate.to = this.disabledStartDate.to
       }
       return unSelectedDate
     },
